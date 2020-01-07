4dd05e2a7273321a5c1dc9de6da64bb877b9aab0
nireus <nireus@ixiaopu.com>
2020-01-07 18:49:28 +0800

    请求首页商品数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,15 +6,121 @@
     <home-swiper :banner="banner"/>
     <recommend-view :recommend="recommend"/>
     <feature-view/>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+      <li>32</li>
+      <li>33</li>
+      <li>34</li>
+      <li>35</li>
+      <li>36</li>
+      <li>37</li>
+      <li>38</li>
+      <li>39</li>
+      <li>40</li>
+      <li>41</li>
+      <li>42</li>
+      <li>43</li>
+      <li>44</li>
+      <li>45</li>
+      <li>46</li>
+      <li>47</li>
+      <li>48</li>
+      <li>49</li>
+      <li>50</li>
+      <li>51</li>
+      <li>52</li>
+      <li>53</li>
+      <li>54</li>
+      <li>55</li>
+      <li>56</li>
+      <li>57</li>
+      <li>58</li>
+      <li>59</li>
+      <li>60</li>
+      <li>61</li>
+      <li>62</li>
+      <li>63</li>
+      <li>64</li>
+      <li>65</li>
+      <li>66</li>
+      <li>67</li>
+      <li>68</li>
+      <li>69</li>
+      <li>70</li>
+      <li>71</li>
+      <li>72</li>
+      <li>73</li>
+      <li>74</li>
+      <li>75</li>
+      <li>76</li>
+      <li>77</li>
+      <li>78</li>
+      <li>79</li>
+      <li>80</li>
+      <li>81</li>
+      <li>82</li>
+      <li>83</li>
+      <li>84</li>
+      <li>85</li>
+      <li>86</li>
+      <li>87</li>
+      <li>88</li>
+      <li>89</li>
+      <li>90</li>
+      <li>91</li>
+      <li>92</li>
+      <li>93</li>
+      <li>94</li>
+      <li>95</li>
+      <li>96</li>
+      <li>97</li>
+      <li>98</li>
+      <li>99</li>
+      <li>100</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
-  import { getHomeMultidata } from 'network/home'
+
+  import { getHomeMultidata, getHomeGoods } from 'network/home'
 
   export default {
     name: 'Home',
@@ -22,23 +128,43 @@
       NavBar,
       HomeSwiper,
       RecommendView,
-      FeatureView
+      FeatureView,
+      TabControl
     },
     data () {
       return {
         banner: [],
-        recommend: []
+        recommend: [],
+        goods: {
+          'pop': {page: 1, list: []},
+          'new': {page: 1, list: []},
+          'sell': {page: 1, list: []},
+        }
       }
     },
     created() {
       // 1. 请求多个数据
-      getHomeMultidata().then(res => {
-        console.log(res)
-        this.banner = res.data.banner.list
-        this.dKeyword = res.data.dKeyword
-        this.keyword = res.data.keyword
-        this.recommend = res.data.recommend.list
-      })
+      this.getHomeMultidata()
+
+      // 2.请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata () {
+        getHomeMultidata().then(res => {
+          this.banner = res.data.banner.list
+          this.recommend = res.data.recommend.list
+        })
+      },
+      getHomeGoods (type) {
+        getHomeGoods(type, this.goods[type].page).then(res => {
+          const goodsList = res.data.list;
+          this.goods[type].list.push(...goodsList)
+          this.goods[type].page += 1
+        })
+      }
     }
   }
 </script>
@@ -55,5 +181,9 @@
     z-index: 9;
     background-color: var(--color-tint);
     color: #fff;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
