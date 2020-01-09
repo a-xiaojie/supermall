86372537c41d61a1a1366a1fcad5f298a4e28153
a-xiaojie <gelouxiaojie@yeah.net>
2020-01-09 15:06:55 +0800

    商品列表组件封装及tabcontrol点击切换

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="goods">
+    <goods-item v-for="(item, index) in goods" :item="item" :key="index"/>
+  </div>
+</template>
+
+<script>
+  import GoodsItem from './GoodsItem'
+  export default {
+    name: 'GoodsList',
+    components: {
+      GoodsItem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    padding: 2px;
+  }
+</style>
